4c4434013cd7810bf66264652f1d47e69aaa1300
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2025-06-05 19:37:46 +0700

    Test auto insert

--- application/controllers/C_Terminasi_Kraksaan.php
+++ application/controllers/C_Terminasi_Kraksaan.php
@@ -16,4 +16,16 @@ class C_Terminasi_Kraksaan extends CI_Controller
         // Simpan data ke tabel 'data_jabatan'
         $this->M_CRUD->insertData($data_jabatan, 'data_jabatan');
     }
+
+    public function Terminasi()
+    {
+        // Data jabatan
+        $data_jabatan = [
+            'nama_jabatan' => 'Test',
+            'created_at'   => date('Y-m-d H:i:s')
+        ];
+
+        // Simpan data ke tabel 'data_jabatan'
+        $this->M_CRUD->insertData($data_jabatan, 'data_jabatan');
+    }
 }
