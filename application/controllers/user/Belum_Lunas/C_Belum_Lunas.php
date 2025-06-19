332042b1a850d3c6a56c2d866b72d5adf49f9762
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2025-06-19 21:20:11 +0700

    Pembayaran perbulan dan perhari tanggal otomatis pada akun user

--- application/controllers/user/Belum_Lunas/C_Belum_Lunas.php
+++ application/controllers/user/Belum_Lunas/C_Belum_Lunas.php
@@ -58,6 +58,11 @@ class C_Belum_Lunas extends CI_Controller
         $year       = $this->session->userdata('tahunGET') ?? $this->session->userdata('tahun');
         $lastDate   = $this->session->userdata('TanggalAkhirGET') ?? $this->session->userdata('TanggalAkhir');
 
+        $this->session->set_userdata([
+            'Bulan_Opsi'        => $month,
+            'Tahun_Opsi'        => $$year
+        ]);
+
         // Untuk ditampilkan (tanpa 0 di depan bulan)
         $bulan_show = $this->session->userdata('bulan_GET') ?? date("n");
         $tahun_show = $this->session->userdata('tahunGET') ?? date("Y");
