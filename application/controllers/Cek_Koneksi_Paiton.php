765406559de6eb4690d37d408f43d6c3e1633cb4
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2025-07-11 14:40:48 +0700

    IP mikrotik

--- application/controllers/Cek_Koneksi_Paiton.php
+++ application/controllers/Cek_Koneksi_Paiton.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Cek_koneksi_paiton extends CI_Controller
+class Cek_Koneksi_Paiton extends CI_Controller
 {
     public function index()
     {
